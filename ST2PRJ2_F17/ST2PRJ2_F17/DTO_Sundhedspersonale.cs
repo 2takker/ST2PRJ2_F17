152b5d86e2c12927e10817d80a34228155e34029
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2017-04-27 11:50:07 +0200

    1. use case samt ST-DTO

---
+++ ST2PRJ2_F17/ST2PRJ2_F17/DTO_Sundhedspersonale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST2PRJ2_F17
+{
+    class DTO_Sundhedspersonale
+    {
+        private string brugerID_ { get; set; }
+        private string kodeord_ { get; set; }
+
+        public DTO_Sundhedspersonale(string brugerID, string kodeord)
+        {
+            brugerID_ = brugerID;
+            kodeord_ = kodeord;
+        }
+        
+    }
+}
---
+++ ST2PRJ2_F17/ST2PRJ2_F17/Logiklag.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB;
+
+namespace Logik
+{
+    class Logiklag
+    {
+        private lokalDB lokDB;
+
+        public void login(DTO_Sundhedspersonale sundhedspersonale)
+        {
+            data.validerLogin(sundhedspersonale);
+        }
+
+        public void importerDatafil()
+        {
+
+        }
+
+        public void gemDatasæt()
+        {
+
+        }
+
+        public string indlæsCPR(string cpr)
+        {            
+            if (validerCPR(cpr) == true)
+                return cpr;
+            else return null;
+        }
+
+        private bool validerCPR(string cpr)
+        {
+            int[] talvægt = { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
+
+            int sum = 0;
+            if (cpr.Length == 10)
+            {
+                for (int i = 0; i < cpr.Length; i++)
+                {
+                    char[] chars = cpr.ToCharArray();
+                    sum += (chars[i] - 0x30) * talvægt[i];
+                }
+                int res = sum % 11;
+                if (res == 0)
+                    return true;
+            }
+            return false;
+        }
+
+
+    }
+}
