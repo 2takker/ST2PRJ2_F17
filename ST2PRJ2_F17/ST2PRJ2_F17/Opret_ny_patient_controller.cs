f7b1440510c7367965b5d7b01f8b412c103a61f7
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2017-05-02 15:20:46 +0200

    Første omgang af DTO_PD samt UC3

---
+++ ST2PRJ2_F17/ST2PRJ2_F17/Opret_ny_patient_controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB;
+using DTO;
+
+namespace Logik
+{
+    class Opret_ny_patient_controller
+    {
+        private lokalDB lokalDB_;
+
+        public Opret_ny_patient_controller()
+        {
+            lokalDB_ = new lokalDB();
+        }
+
+        private bool validerCPR(string cpr)
+        {
+            int[] talVægt = { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
+
+            int sum = 0;
+            if (cpr.Length == 10)
+            {
+                for (int i = 0; i < cpr.Length; i++)
+                {
+                    char[] chars = cpr.ToCharArray();
+                    sum += (chars[i] - 0x30) * talVægt[i];
+                }
+                int res = sum % 11;
+                if (res == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool gemPatientData(DTO_PatientData patientData)
+        {
+            if (validerCPR(patientData.CPRNummer_) == false)
+            {
+                return false;
+            }
+
+            else
+            {
+                
+                if (lokalDB_.findPatient(patientData) == false)
+                {
+
+                }
+
+            }                
+        }
+        
+    }
+}
