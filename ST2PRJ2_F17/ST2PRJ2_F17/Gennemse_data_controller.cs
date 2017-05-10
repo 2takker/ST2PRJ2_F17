74a8d7562a60bcd7a59fea606c7468b0d7c35ab2
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2017-05-10 13:32:54 +0200

    UC4 klar til merge

--- ST2PRJ2_F17/ST2PRJ2_F17/Gennemse_data_controller.cs
+++ ST2PRJ2_F17/ST2PRJ2_F17/Gennemse_data_controller.cs
@@ -14,13 +14,17 @@ namespace ST2PRJ2_F17
         private DTO_Datasæt dtoDatasæt_;
         private List<DTO_Datasæt> dtoDatasætList_;
         private DTO_PatientData dtoPD_;
-        
+        private List<double> ipListe_;
+        private List<double> analyserDataListe_;
+        private int index_;
+
         public Gennemse_data_controller()
         {
             lokalDB_ = new lokalDB();
             dtoDatasæt_ = new DTO_Datasæt();
             dtoDatasætList_ = new List<DTO_Datasæt>();
             dtoPD_ = new DTO_PatientData();
+            ipListe_ = new List<double>();
         }
 
         public bool validerCPR(string cpr)
@@ -58,11 +62,101 @@ namespace ST2PRJ2_F17
 
         public List<DTO_Datasæt> visPatientData(string cpr)
         {
-            dtoDatasætList_  == lokalDB_.hentData(cpr);
-            {                
-                return dtoDatasætList_;
+                         
+        }
+
+        
+        public bool analyserValgtDatasæt(int index)
+        {
+            index_ = index;
+            analyserDataListe_ = dtoDatasætList_[index].Data_;
+
+            return true;         
+        }
+
+        public List<double> hentIP()
+        {
+            ipListe_ = dtoDatasætList_[index_].Ip_;
+            return ipListe_;
+        }
+        public List<double> hentAnalyseretDatasæt()
+        {
+            return analyserDataListe_;
+        }
+
+        public List<double> findRR()
+        {
+            List<double> toppunktsListeX = findToppunkt(analyserDataListe_);
+
+            for (int i = 0; i < (toppunktsListeX.Count - 1); i++)
+            {
+                if ((toppunktsListeX[i + 1] - toppunktsListeX[i]) > 500)
+                {
+                    ipListe_.Add(toppunktsListeX[i]);
+                }
             }
-             
+            return ipListe_;
+        }
+
+        public List<double> findToppunkt(List<double> inputListe)
+        {
+            List<double> toppunktsListe = new List<double>();
+            double maxValue = 0;
+
+
+            List<double> last10 = new List<double>();
+            for (int i = 0; i < inputListe.Count; i++)
+            {
+                last10.Add(inputListe[i]);
+                if (last10.Count > 10)
+                {
+                    last10.RemoveAt(0);
+                }
+
+                if (overLimit(last10, 1.0))
+                {
+                    if (inputListe[i] >= maxValue)
+                    {
+                        maxValue = inputListe[i];
+                    }
+                }
+                else
+                {
+                    if (maxValue > 0)
+                    {
+                        toppunktsListe.Add(i);
+                        maxValue = 0;
+                    }
+                }
+            }
+
+            return toppunktsListe;
+        }
+
+        public bool overLimit(List<double> values, double limit)
+        {
+            double sum = 0;
+            if (values.Count < 10)
+            {
+                return false;
+            }
+
+            foreach (var item in values)
+            {
+                sum += item;
+            }
+
+            double avg = (sum / values.Count);
+
+            if (avg > limit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
