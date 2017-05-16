079bfe916f2b5e7ee8348b2cded296ac5ffd0cf1
2takker <2takker@gmail.com>
2017-05-16 11:06:48 +0200

    Scale test 2

--- ST2PRJ2_F17/ST2PRJ2_F17/Preview.cs
+++ ST2PRJ2_F17/ST2PRJ2_F17/Preview.cs
@@ -26,9 +26,9 @@ namespace Præsentationslag
             InitializeComponent();
 
             Rectangle screen = Screen.PrimaryScreen.WorkingArea;
-            int w = Width >= screen.Width ? screen.Width : (screen.Width + Width)/2;
-            int h = Height >= screen.Height ? screen.Height : (screen.Height + Height) / 4+20;
-            this.Location = new Point((screen.Width - w) / 2, (screen.Height - h) / 4+20);
+            int w = Width >= screen.Width ? screen.Width : (screen.Width + Width*3)/3;
+            int h = Height >= screen.Height ? screen.Height : (screen.Height + Height/2) /3;
+            this.Location = new Point((screen.Width - w) / 2, (screen.Height - h) / 3);
             this.Size = new Size(w, h);
 
             previewData.Width = w-150;
