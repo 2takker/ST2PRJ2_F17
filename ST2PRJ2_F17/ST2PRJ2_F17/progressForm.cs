982de1851c20e842bc3dd5c8536f5797fc707ef2
Bjarke Hansen <2takker@gmail.com>
2017-05-18 22:26:47 +0200

    Added class for progress bar

---
+++ ST2PRJ2_F17/ST2PRJ2_F17/progressForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progress
+{
+    public partial class progressForm : Form
+    {        
+        public progressForm(string title, string textAboveBar, int max)
+        {
+            InitializeComponent();
+
+            Title_ = title;
+            TextAboveBar_ = textAboveBar;
+            Maximum_ = max;            
+        }
+
+        public int Maximum_
+        {
+            get { return prgrsBar.Maximum; }
+            set
+            {
+                prgrsBar.Maximum = value;
+            }
+        }
+
+        public string Title_
+        {
+            set
+            {
+                Text = value;
+            }
+        }
+        
+        public string TextAboveBar_
+        {
+            set
+            {
+                lblProces.Text = value;
+            }
+        }
+
+        public int CurrentValue_
+        {
+            get
+            {
+                return prgrsBar.Value;
+            }
+        }
+
+        public void increaseProgress(int amount)
+        {
+            prgrsBar.Increment(amount);
+        }
+
+        public void decreaseProgress(int amount)
+        {
+            prgrsBar.Increment(-amount);
+        }
+
+        public void openProgressForm()
+        {            
+            Show();
+        }
+
+        public void closeProgressForm()
+        {
+            Hide();
+            prgrsBar.Value = 0;
+        }
+
+        public bool isAtMaX()
+        {
+            if(CurrentValue_ == Maximum_)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ST2PRJ2_F17/ST2PRJ2_F17/progressForm.designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Progress
+{
+    partial class progressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgrsBar = new System.Windows.Forms.ProgressBar();
+            this.lblProces = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prgrsBar
+            // 
+            this.prgrsBar.Location = new System.Drawing.Point(12, 16);
+            this.prgrsBar.Name = "prgrsBar";
+            this.prgrsBar.Size = new System.Drawing.Size(264, 23);
+            this.prgrsBar.TabIndex = 0;
+            this.prgrsBar.UseWaitCursor = true;
+            // 
+            // lblProces
+            // 
+            this.lblProces.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.lblProces.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblProces.Location = new System.Drawing.Point(0, 0);
+            this.lblProces.Name = "lblProces";
+            this.lblProces.Size = new System.Drawing.Size(288, 13);
+            this.lblProces.TabIndex = 1;
+            this.lblProces.Text = "textAboveBar";
+            this.lblProces.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblProces.UseWaitCursor = true;
+            // 
+            // progressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 51);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblProces);
+            this.Controls.Add(this.prgrsBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "progressForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Title";
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgrsBar;
+        private System.Windows.Forms.Label lblProces;
+    }
+}
